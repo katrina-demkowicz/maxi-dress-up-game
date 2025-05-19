70e9f29e4242e15caf1c7a6fe4f2d8b96b923ab8
katrina-demkowicz <kpd8178@g.rit.edu>
2025-05-19 01:03:48 -0400

    render initial doll

--- game/src/Page.js
+++ game/src/Page.js
@@ -6,16 +6,16 @@ class Page extends Component
 
     doll = {
         "body": "./assets/DollParts/Body/Skin5.PNG",
-        "eyes": "./src/assets/DollParts/Eyes/EyesKimiko.PNG",
-        "brows": "./src/assets/DollParts/Eyebrows/BrowsLydia.PNG",
-        "nose": "./src/assets/DollParts/Noses/NoseKimiko.PNG",
-        "lips": "./src/assets/DollParts/Lips/Lipsbrown.PNG",
+        "eyes": "./assets/DollParts/Eyes/EyesKimiko.PNG",
+        "brows": "./assets/DollParts/Eyebrows/BrowsLydia.PNG",
+        "nose": "./assets/DollParts/Noses/NoseKimiko.PNG",
+        "lips": "./assets/DollParts/Lips/Lipsbrown.PNG",
         "faceDetails": "",
         "glasses": "",
         "necklace": "",
-        "dress": "",
+        "dress": "./assets/DollParts/Dresses/Dressstarter.PNG",
         "shoes": "",
-        "hair": ""
+        "hair": "./assets/DollParts/Hair/TheThelma/ThelmaGinger.PNG"
     }
 
     constructor(props)
@@ -32,6 +32,25 @@ class Page extends Component
             <div>
                 <p>hello.</p>
                 <img className="Doll" src={require(`${this.state.dollData.body}`)} alt="body" />
+                <img className="Doll" src={require(`${this.state.dollData.dress}`)} alt="dress" />
+                <img className="Doll" src={require(`${this.state.dollData.eyes}`)} alt="eyes" />
+                <img className="Doll" src={require(`${this.state.dollData.brows}`)} alt="brows" />
+                <img className="Doll" src={require(`${this.state.dollData.nose}`)} alt="nose" />
+                <img className="Doll" src={require(`${this.state.dollData.lips}`)} alt="lips" />
+                <img className="Doll" src={require(`${this.state.dollData.hair}`)} alt="hair" />
+                {/* empty string attributes */}
+                {this.state.dollData.faceDetails != "" &&
+                    <img className="Doll" src={require(`${this.state.dollData.faceDetails}`)} alt="face details" />
+                }
+                {this.state.dollData.glasses != "" &&
+                    <img className="Doll" src={require(`${this.state.dollData.glasses}`)} alt="glasses" />
+                }
+                {this.state.dollData.necklace != "" &&
+                    <img className="Doll" src={require(`${this.state.dollData.necklace}`)} alt="necklace" />
+                }
+                {this.state.dollData.shoes != "" &&
+                    <img className="Doll" src={require(`${this.state.dollData.shoes}`)} alt="shoes" />
+                }
             </div>
         )
     }
