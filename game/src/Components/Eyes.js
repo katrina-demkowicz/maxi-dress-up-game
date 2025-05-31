92b1545f3357a356a5a46cb76326e11c862629c4
katrina-demkowicz <kpd8178@g.rit.edu>
2025-05-30 21:17:19 -0400

    Moved assets into public folder
    Made changes in components to accomodate this change.

--- game/src/Components/Eyes.js
+++ game/src/Components/Eyes.js
@@ -1,16 +1,17 @@
 
 const Eyes = (props) =>
 {
+
     const eyes = [
-        "../assets/DollParts/Eyes/EyesCosmina.PNG",
-        "../assets/DollParts/Eyes/EyesGyaru.PNG",
-        "../assets/DollParts/Eyes/EyesImogen.PNG",
-        "../assets/DollParts/Eyes/EyesKimiko.PNG",
-        "../assets/DollParts/Eyes/EyesLacramioara.PNG",
-        "../assets/DollParts/Eyes/EyesMargareta.PNG",
-        "../assets/DollParts/Eyes/EyesSonya.PNG",
-        "../assets/DollParts/Eyes/EyesTradgoth.PNG",
-        "../assets/DollParts/Eyes/EyesUna.PNG"
+        "/assets/DollParts/Eyes/EyesCosmina.PNG",
+        "/assets/DollParts/Eyes/EyesGyaru.PNG",
+        "/assets/DollParts/Eyes/EyesImogen.PNG",
+        "/assets/DollParts/Eyes/EyesKimiko.PNG",
+        "/assets/DollParts/Eyes/EyesLacramioara.PNG",
+        "/assets/DollParts/Eyes/EyesMargareta.PNG",
+        "/assets/DollParts/Eyes/EyesSonya.PNG",
+        "/assets/DollParts/Eyes/EyesTradgoth.PNG",
+        "/assets/DollParts/Eyes/EyesUna.PNG"
     ]
 
     const saveChange = (eye) =>
@@ -23,7 +24,7 @@ const Eyes = (props) =>
             {/*The div tag is temporary, I would like to find a better solution to have the images next to 
             each other without there being space inbetween them.*/}
             {eyes.map((eye, i) => {
-                return (<div className='Image'><img className="Eyes" src={require(`${eye}`)} alt={eye} key={i} onClick={() => saveChange(eye)} ></img></div>)
+                return (<div className='Image'><img className="Eyes" src={eye} alt={eye} key={i} onClick={() => saveChange(eye)} ></img></div>)
             })}
         </div>
     )
--- game/src/Page.js
+++ game/src/Page.js
@@ -6,17 +6,17 @@ function Page()
 {
 
     const [doll, setDoll]= useState({
-        "body": "./assets/DollParts/Body/Skin5.PNG",
-        "eyes": "./assets/DollParts/Eyes/EyesKimiko.PNG",
-        "brows": "./assets/DollParts/Eyebrows/BrowsLydia.PNG",
-        "nose": "./assets/DollParts/Noses/NoseKimiko.PNG",
-        "lips": "./assets/DollParts/Lips/Lipsbrown.PNG",
+        "body": "/assets/DollParts/Body/Skin5.PNG",
+        "eyes": "/assets/DollParts/Eyes/EyesKimiko.PNG",
+        "brows": "/assets/DollParts/Eyebrows/BrowsLydia.PNG",
+        "nose": "/assets/DollParts/Noses/NoseKimiko.PNG",
+        "lips": "/assets/DollParts/Lips/Lipsbrown.PNG",
         "faceDetails": "",
         "glasses": "",
         "necklace": "",
-        "dress": "./assets/DollParts/Dresses/Dressstarter.PNG",
+        "dress": "/assets/DollParts/Dresses/Dressstarter.PNG",
         "shoes": "",
-        "hair": "./assets/DollParts/Hair/TheThelma/ThelmaGinger.PNG"
+        "hair": "/assets/DollParts/Hair/TheThelma/ThelmaGinger.PNG"
     });
 
     const updateCharacter = (prop, asset) =>
@@ -36,25 +36,25 @@ function Page()
     else {
         return(
             <div className='Game'>
-                <img className="Doll" src={require(`${doll.body}`)} alt="body" />
-                <img className="Doll" src={require(`${doll.dress}`)} alt="dress" />
-                <img className="Doll" src={require(`${doll.eyes}`)} alt="eyes" />
-                <img className="Doll" src={require(`${doll.brows}`)} alt="brows" />
-                <img className="Doll" src={require(`${doll.nose}`)} alt="nose" />
-                <img className="Doll" src={require(`${doll.lips}`)} alt="lips" />
-                <img className="Doll" src={require(`${doll.hair}`)} alt="hair" />
+                <img className="Doll" src={doll.body} alt="body" />
+                <img className="Doll" src={doll.dress} alt="dress" />
+                <img className="Doll" src={doll.eyes} alt="eyes" />
+                <img className="Doll" src={doll.brows} alt="brows" />
+                <img className="Doll" src={doll.nose} alt="nose" />
+                <img className="Doll" src={doll.lips} alt="lips" />
+                <img className="Doll" src={doll.hair} alt="hair" />
                 {/* empty string attributes */}
                 {doll.faceDetails != "" &&
-                    <img className="Doll" src={require(`${doll.faceDetails}`)} alt="face details" />
+                    <img className="Doll" src={doll.faceDetails} alt="face details" />
                 }
                 {doll.glasses != "" &&
-                    <img className="Doll" src={require(`${doll.glasses}`)} alt="glasses" />
+                    <img className="Doll" src={doll.glasses} alt="glasses" />
                 }
                 {doll.necklace != "" &&
-                    <img className="Doll" src={require(`${doll.necklace}`)} alt="necklace" />
+                    <img className="Doll" src={doll.necklace} alt="necklace" />
                 }
                 {doll.shoes != "" &&
-                    <img className="Doll" src={require(`${doll.shoes}`)} alt="shoes" />
+                    <img className="Doll" src={doll.shoes} alt="shoes" />
                 }
                 <Eyes callback={updateCharacter}/>
             </div>
